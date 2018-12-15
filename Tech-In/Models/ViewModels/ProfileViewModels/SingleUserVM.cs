48359324d32675f9aac55f9077b58c1ffb39e2c1
fawad1997 <fawad_12@outlook.com>
2018-12-15 22:17:46 +0500

    works perfect without user page added

---
+++ Tech-In/Models/ViewModels/ProfileViewModels/SingleUserVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tech_In.Models.ViewModels.ProfileViewModels
+{
+    public class SingleUserVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfileImage { get; set; }
+        public string UserName { get; set; }
+    }
+}
